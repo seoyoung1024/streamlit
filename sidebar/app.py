0ee77030658c11fc4d2f817d73235240746f08f0
seoyoung1024 <jjssyy0608@gmail.com>
2025-06-02 05:40:16 +0000

    up

--- sidebar/app.py
+++ sidebar/app.py
@@ -1,8 +1,8 @@
 import streamlit as st
-from PIL import Image
+from PIL import Image 
 
-# -------------------- side bar --------------------
-st.sidebar.title('Sidebar')
+#----------------------side bar-----------------------------
+st.sidebar.title('SideBar')
 st.sidebar.header('텍스트 입력 사용 예')
 user_id = st.sidebar.text_input('아이디(ID) 입력', value='root', max_chars=15)
 user_password = st.sidebar.text_input('패스워드(Password) 입력', value='1234', type='password')
@@ -10,17 +10,16 @@ user_password = st.sidebar.text_input('패스워드(Password) 입력', value='12
 st.sidebar.header('셀렉트 박스 사용 예')
 selectbox_option = ['진주 귀걸이를 한 소녀', '별이 빛나는 밤', '절규', '월하정인']
 your_option = st.sidebar.selectbox('좋아하는 작품은?', selectbox_option, index=3)
-st.sidebar.write('**당신이 선택**: ', your_option)
+st.sidebar.write('**당신의 선택** : ', your_option)
 
-# -------------------- Main view --------------------
-
+#----------------------Main view-----------------------------
 folder = './data/'
 
 image_files = [
     'https://github.com/seoyoung1024/streamlit/blob/main/sidebar/data/Vermeer.png?raw=true',
     'https://github.com/seoyoung1024/streamlit/blob/main/sidebar/data/Gogh.png?raw=true',
     'https://github.com/seoyoung1024/streamlit/blob/main/sidebar/data/Munch.png?raw=true',
-    'https://github.com/seoyoung1024/streamlit/blob/main/sidebar/data/ShinYoonBok.png?raw=true'
+    'https://github.com/seoyoung1024/streamlit/blob/main/sidebar/data/ShinYoonbok.png?raw=true',
 ]
 
 selectbox_options_index = selectbox_option.index(your_option)
